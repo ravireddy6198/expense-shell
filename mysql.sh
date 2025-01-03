f8f2eb1c66bdcdf7afa812747a205a7e7c237ace
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-03 13:51:47 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -7,6 +7,17 @@ LOG_FILE=$( echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d--%H:%M:%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE.log------$TIMESTAMP"
 
+#################################################
+
+dnf list installed mysql
+if [ $? -eq 0 ]
+then    
+    dnf remove mysql
+else
+    exit 0
+fi
+##################################################
+
 
 VALIDATE(){
      if [ $1 -ne 0 ]
@@ -18,21 +29,22 @@ VALIDATE(){
     fi
 }
 
-dnf list installed mysql
-if [ $? -eq 0 ]
-then    
-    dnf remove mysql
-else
-    exit 0
-fi
-
-if [ $USERID -ne 0 ]
-then
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1
-fi
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+
+
 
 dnf list installed mysql &>>$LOG_FILE_NAME
+
+CHECK_ROOT
 
 if [ $? -ne 0 ]
 then

c2c2312da88bb7ce4309517751426f678441ccc0
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-03 11:59:02 +0530

    expense-shell

---
+++ mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0]
+then
+    dnf install mysql
+    if [ $? -ne 0 ]
+    then
+        echo "server failure"
+    else
+        echo "server success"
+    fi
+else
+    echo "server already"
+    
+

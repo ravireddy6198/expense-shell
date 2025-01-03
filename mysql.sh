b0da6b51781cf79611fa5e1835b6e1d03c254a10
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-03 14:56:22 +0530

    expense-shell

--- mysql.sh
+++ mysql.sh
@@ -4,61 +4,59 @@ echo " First"
 dnf list installed mysql
 
 
-if [ $? -eq 90 ]
+if [ $? -eq 0 ]
 then 
     echo "First One"
     dnf remove mysql  
-else 
-    exit 0
 fi
 
 echo "hiiiiiiiiiii"
 
-# echo " Second"
-
-# USERID=$(id -u)
-
-# LOG_FOLDER="/var/log/mysql-logs"
-# LOG_FILE=$( echo $0 | cut -d "." -f1 )
-# TIMESTAMP=$(date +%Y-%m-%d--%H:%M:%S)
-# LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE.log------$TIMESTAMP"
 
 
+USERID=$(id -u)
 
-# echo "Three"
-
-# VALIDATE(){
-#      if [ $1 -ne 0 ]
-#     then
-#         echo "$2 failure"
-#         exit 1
-#     else
-#         echo "$2 success"
-#     fi
-# }
-
-# CHECK_ROOT(){
-#     if [ $USERID -ne 0 ]
-#     then
-#     echo "ERROR:: You must have sudo access to execute this script"
-#     exit 1
-#     fi
-# }
-
-# echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+LOG_FOLDER="/var/log/mysql-logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d--%H:%M:%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE.log------$TIMESTAMP"
 
 
 
+echo "Three"
 
-# dnf list installed mysql &>>$LOG_FILE_NAME
+VALIDATE(){
+     if [ $1 -ne 0 ]
+    then
+        echo "$2 failure"
+        exit 1
+    else
+        echo "$2 success"
+    fi
+}
 
-# CHECK_ROOT
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1
+    fi
+}
 
-# if [ $? -ne 0 ]
-# then
-#     dnf install mysql -y &>>$LOG_FILE_NAME
-#     VALIDATE $? " MYSQL SERVER "
-# else
-#     echo "MYSQL SERVER installed already"
-# fi
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? " MYSQL SERVER "
+else
+    echo "MYSQL SERVER installed already"
+fi
 
